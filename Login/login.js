3150fb6d26b86ceb7255988b5e281fc90c8f3ee4
Jayson Isaac <jayson.isaac1@gmail.com>
2013-11-30 14:13:06 -0500

    Login Page

---
+++ Login/login.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+Parse.initialize("BIKAjoie2DfWo8w3022C7kgXlJpUoYFWv8Kwsoog", "cOqVO2RyFUDLBtfv5TzmpEZHsg5ThCdXnOtQrxsw");
+
+$('#submit').click(function(){
+  console.log("Jayson");
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  Parse.User.logIn(username, password, {
+  success: function(user) {
+    alert("Logged in!")
+  },
+  error: function(user, error) {
+    alert("Incorrect Credentials.")
+  }
+});
+});
+});
